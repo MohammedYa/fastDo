c250b5347cc87dd6b39608d0748826132e9ea91d
Eman Ahmed <emanamra1999@gmail.com>
2023-08-18 01:57:18 +0300

    home-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxSplideModule } from 'ngx-splide';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,7 +35,8 @@ import { FilterPipe } from './core/pipes/filter.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSplideModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import Splide from '@splidejs/splide';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   
- 
+  
+  ngOnInit(): void {
+  // new Splide( '.splide' ).mount();
+    // var splide = new Splide( '.splide', {
+    //   type   : 'loop',
+    //   drag   : 'free',
+    //   snap   : true,
+    //   perPage: 3,
+    //   rewind : true,
+    // } );
+    
+    // splide.mount();
+}
+
+
+
+categories: any[] = [
+  {categoryName: 'Oral Druggs', categoryImgPath:'../../../assets/pills.jpg' },
+  {categoryName: 'Oral Druggs', categoryImgPath:'../../../assets/pills.jpg' },
+  {categoryName: 'Oral Druggs', categoryImgPath:'../../../assets/pills.jpg' },
+  {categoryName: 'Oral Druggs', categoryImgPath:'../../../assets/pills.jpg' },
+  {categoryName: 'Oral Druggs', categoryImgPath:'../../../assets/pills.jpg' },
+  {categoryName: 'Oral Druggs', categoryImgPath:'../../../assets/pills.jpg' },
+]
+
+slides: any[] = [
+  {distributorName: 'Karma'},
+  {distributorName: 'AliExpress'},
+  {distributorName: 'Dalida'},
+  {distributorName: 'Elsadek'},
+  {distributorName: 'Eman'},
+  {distributorName: 'Raheek'},
+]
+
+
 }
